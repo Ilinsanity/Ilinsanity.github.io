51c74689f47ad798220db2a80d941ce732a2e757
Ilinsanity <Issaclin2002@gmail.com>
2022-02-03 01:44:46 -0500

    added an animation to the name

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-function textreplace(i) {
-    var text = ['Web Developer', 'Programmer', 'Raptors Fan', 'University Student'];
+// function textreplace(i) {
+//     var text = ['Web Developer', 'Programmer', 'Raptors Fan', 'University Student'];
 
-    if (text.length > i) {
-        setTimeout(function() {
-            document.getElementById("middlelanding").innerHTML = text[i];
-            textreplace(++i);
-        }, 2000)
+//     if (text.length > i) {
+//         setTimeout(function() {
+//             $(".middlelanding").text = text[i];
+//             textreplace(++i);
+//         }, 2000)
 
-    } else if (text.length == i) {
-        textreplace(0);
-    }
-}
+//     } else if (text.length == i) {
+//         textreplace(0);
+//     }
+// }
+// textreplace(0);
 
-textreplace(0);
+$(".name").mouseenter(function() {
+    // if ($(".name").hasClass("nameanimation")) {
+    //     $(".name").removeClass("nameanimation");
+    //     $(".name").addClass("nameanimation2");
+    // } else if ($(".name").hasClass("nameanimation2")) {
+    //     $(".name").removeClass("nameanimation2");
+    //     $(".name").addClass("nameanimation");
+    // } else {
+    //     $(".name").addClass("nameanimation");
+    // }
+    $(".name").removeClass("nameanimation2");
+    $(".name").addClass("nameanimation");
+});
+
+$(".name").mouseleave(function() {
+    // if ($(".name").hasClass("nameanimation")) {
+    //     $(".name").removeClass("nameanimation");
+    //     $(".name").addClass("nameanimation2");
+    // } else if ($(".name").hasClass("nameanimation2")) {
+    //     $(".name").removeClass("nameanimation2");
+    //     $(".name").addClass("nameanimation");
+    // } else {
+    //     $(".name").addClass("nameanimation");
+    // }
+    $(".name").removeClass("nameanimation");
+    $(".name").addClass("nameanimation2");
+});

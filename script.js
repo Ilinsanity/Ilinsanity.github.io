26fa0e176da5e9b69d573ae769aca5c3cf93fd46
Ilinsanity <Issaclin2002@gmail.com>
2025-05-17 17:31:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-
-
-$(".name").mouseenter(function() {
-  
-    $(".name").removeClass("nameanimation2");
-    $(".name").addClass("nameanimation");
+$(".name").mouseenter(function () {
+  $(".name").removeClass("nameanimation2");
+  $(".name").addClass("nameanimation");
 });
 
-$(".name").mouseleave(function() {
-   
-    $(".name").removeClass("nameanimation");
-    $(".name").addClass("nameanimation2");
+$(".name").mouseleave(function () {
+  $(".name").removeClass("nameanimation");
+  $(".name").addClass("nameanimation2");
 });
 setTimeout(() => {
-    document.getElementById("namea").style.visibility = "visible";
-var text = document.getElementById('namea');
+  document.getElementById("namea").style.visibility = "visible";
+  var text = document.getElementById("namea");
 
-var typewriter = new Typewriter(text, {
-    loop: true
-});
+  var typewriter = new Typewriter(text, {
+    loop: true,
+  });
 
-typewriter.typeString('Issac Lin')
+  typewriter
+    .typeString("Issac Lin")
     .pauseFor(1500)
     .deleteAll()
-    .typeString('A Student')
+    .typeString("A New Grad")
     .pauseFor(1500)
     .deleteAll()
-    .typeString('A Web Developer')
+    .typeString("A Web Developer")
     .pauseFor(1500)
     .deleteAll()
-    .typeString('A Raptors Fan')
+    .typeString("A Raptors Fan")
     .pauseFor(1500)
     .start();
 }, 2700);
-
